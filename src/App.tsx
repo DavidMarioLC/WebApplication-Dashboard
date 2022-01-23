b4174755524a5d9b20b3998371c89fa76d93a2df
DavidMarioLC <davidmariolc.dev@gmail.com>
2022-01-22 22:53:43 -0500

    feat(app): add config initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import Courses from './pages/Courses';
+import NewCourse from './components/NewCourse';
+import Clases from './components/Clases';
+import NewClases from './components/NewClases';
+import Modules from './components/Modules';
+import NewModules from './components/NewModules';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route path='/' element={<Navigate to='/cursos' replace />} />
+        <Route path='cursos' element={<Courses />} />
+        <Route path='cursos/nuevo' element={<NewCourse />} />
+        <Route path='clases' element={<Clases />} />
+        <Route path='clases/nuevo' element={<NewClases />} />
+        <Route path='modulos' element={<Modules />} />
+        <Route path='modulos/nuevo' element={<NewModules />} />
+      </Route>
+    </Routes>
   );
 }
 
